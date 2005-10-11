92b38ccfbd1506db341f8bffaf0bcc7fd3ed9024
Matthias Gamer <gamer@uni-mainz.de>
2005-10-11 00:00:00 +0000

    version 0.61

--- R/print.icclist.R
+++ R/print.icclist.R
@@ -7,10 +7,11 @@ function(x, ...)
 	cat(paste("   Type :", x$type, "\n\n"))
 	cat(paste("   Subjects =", x$subjects, "\n"))
 	cat(paste("     Raters =", x$raters, "\n"))
-	results <- paste(format.char(x$icc.name, width=11, flag="+"), "=", format(x$value, digits=3))
+	results <- paste(formatC(x$icc.name, width=11, flag="+"), "=", format(x$value, digits=3))
 	cat(results)
   cat("\n\n F-Test, H0: r0 =",x$r0,"\n")
-	Ftest <- paste(format.char(paste("F(",x$df1,",",format(x$df2, digits=3),")",sep=""), width=11, flag="+"), "=", format(x$Fvalue, digits=3),
+	Ftest <- paste(formatC(paste("F(",x$df1,",",format(x$df2, digits=3),")",sep=""), width=11, flag="+"), "=",
+                                              format(x$Fvalue, digits=3),
 	               ", p =", format(x$p.value, digits=3), "\n\n")
   cat(Ftest)
 	cat(" ", round(x$conf.level*100,digits=1), "%-Confidence Interval for ICC Population Values:\n", sep="")
--- R/print.irrlist.R
+++ R/print.irrlist.R
@@ -4,10 +4,10 @@ function(x, ...)
   cat(" ", x$method, "\n\n",sep="")
 	cat(paste(" Subjects =", x$subjects, "\n"))
 	cat(paste("   Raters =", x$raters, "\n"))
-	results <- paste(format.char(x$irr.name, width=9, flag="+"), "=", format(x$value, digits=3),"\n")
+	results <- paste(formatC(x$irr.name, width=9, flag="+"), "=", format(x$value, digits=3),"\n")
 	cat(results)
 	if (!is.null(x$statistic)) {
-  	statistic <- paste(format.char(x$stat.name, width=9, flag="+"), "=", format(x$statistic, digits=3), "\n")
+  	statistic <- paste(formatC(x$stat.name, width=9, flag="+"), "=", format(x$statistic, digits=3), "\n")
   	cat("\n", statistic, sep="")
   	cat(paste("  p-value =", format(x$p.value, digits=3), "\n"))
 	}
